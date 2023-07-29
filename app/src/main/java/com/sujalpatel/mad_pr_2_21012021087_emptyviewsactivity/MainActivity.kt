31d0df33a40f319fbd092797f866bf8c765c8f93
Sujal Patel <sujalpatel1882@gmail.com>
2023-07-29 09:49:05 +0530

    pr-2

--- app/src/main/java/com/sujalpatel/mad_pr_2_21012021087_emptyviewsactivity/MainActivity.kt
+++ app/src/main/java/com/sujalpatel/mad_pr_2_21012021087_emptyviewsactivity/MainActivity.kt
@@ -1,16 +1,27 @@
- package com.sujalpatel.mad_pr_2_21012021087_emptyviewsactivity
+package com.sujalpatel.mad_pr_2_21012021087_emptyviewsactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
  class MainActivity : AppCompatActivity() {
      val TAG="MainActivity";
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         showMesssage("On create method is called")
+        var textview= findViewById<TextView>(R.id.text_view)
+        textview.setOnClickListener(View.OnClickListener {
+            val intent=Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        })
+
 
     }
      fun showMesssage(message:String){
@@ -27,5 +38,25 @@ import android.widget.Toast
      override fun onResume() {
          super.onResume()  
          showMesssage("onResume method is called")
+     }
+
+     override fun onPause() {
+         super.onPause()
+         showMesssage("onPause method is called")
+     }
+
+     override fun onRestart() {
+         super.onRestart()
+         showMesssage("onRestart method called")
+     }
+
+     override fun onStop() {
+         super.onStop()
+         showMesssage("onStop method is called")
+     }
+
+     override fun onDestroy() {
+         super.onDestroy()
+         Toast.makeText(this, "onDestroy method is called", Toast.LENGTH_SHORT).show()
      }
 }
---
+++ app/src/main/java/com/sujalpatel/mad_pr_2_21012021087_emptyviewsactivity/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.sujalpatel.mad_pr_2_21012021087_emptyviewsactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
