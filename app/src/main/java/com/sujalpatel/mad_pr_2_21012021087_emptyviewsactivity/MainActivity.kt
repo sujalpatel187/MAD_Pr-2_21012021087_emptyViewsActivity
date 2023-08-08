5fe92bd302cf947d48ad20e92ec06ddf08ca4777
Sujal Patel <sujalpatel1882@gmail.com>
2023-08-08 21:38:53 +0530

    pr-2

--- app/src/main/java/com/sujalpatel/mad_pr_2_21012021087_emptyviewsactivity/MainActivity.kt
+++ app/src/main/java/com/sujalpatel/mad_pr_2_21012021087_emptyviewsactivity/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.sujalpatel.mad_pr_2_21012021087_emptyviewsactivity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet.Constraint
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
